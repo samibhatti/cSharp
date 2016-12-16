5b13c869d15c3d8463e46ffb16177c9adc0c5a2b
samibhatti <abdulsami.bhatti@gmail.com>
2016-12-16 12:44:55 +0100

    1-10

---
+++ Projects/ConsoleApplication1-10/ConsoleApplication1-10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1_9
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Task<int> t = Task.Run(() =>
+            {
+                return 42;
+
+            }).ContinueWith((i) =>
+            {
+                return i.Result * 2; //continuation of another task as soon as the first finishes
+            });
+
+            Console.WriteLine(t.Result); // Display 84
+
+            Console.ReadKey();
+        }
+    }
+}
