7fd30065f96bf3ae85e19b70413f9b789aa4b930
samibhatti <abdulsami.bhatti@gmail.com>
2016-12-16 12:56:29 +0100

    1-11

---
+++ Projects/ConsoleApplication1-11/ConsoleApplication1-11/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1_11
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Task<int> t = Task.Run(() =>
+            {
+                return 42;
+
+            });
+            
+            t.ContinueWith((i) =>
+            {
+                Console.WriteLine("Canceled");
+            }, TaskContinuationOptions.OnlyOnCanceled);
+
+            t.ContinueWith((i) =>
+            {
+                Console.WriteLine("Faulted");
+
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            var completedTask = t.ContinueWith((i) =>
+
+            {
+                Console.WriteLine("Completed");
+            }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            completedTask.Wait();
+
+            Console.ReadKey();
+        }
+    }
+}
