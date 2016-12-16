45f9cd3c353dabf2ef45cdaca90ae958cf996ff7
samibhatti <abdulsami.bhatti@gmail.com>
2016-12-16 12:05:42 +0100

    1-8

---
+++ Projects/ConsoleApplication1-8/ConsoleApplication1-8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1_8
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Task t = Task.Run(() =>
+            {
+                for (int x = 0; x < 100; x++)
+                {
+                    Console.Write('*');
+                }
+            });
+
+            t.Wait();
+            Console.ReadKey();
+        }
+
+    }
+}
