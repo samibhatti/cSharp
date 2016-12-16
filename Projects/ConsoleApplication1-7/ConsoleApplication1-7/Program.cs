a8aafd519c5071925e8d45249adfa4487cc7df55
samibhatti <abdulsami.bhatti@gmail.com>
2016-12-16 11:55:41 +0100

    1-7

---
+++ Projects/ConsoleApplication1-7/ConsoleApplication1-7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication5
+{
+    public static class Program
+    {
+        
+        public static void Main()
+        {
+                ThreadPool.QueueUserWorkItem((s) =>
+                {
+                   
+                    Console.WriteLine("Working on a thread from threadpool");
+                });
+
+            Console.ReadLine();
+        }
+
+    }
+}
