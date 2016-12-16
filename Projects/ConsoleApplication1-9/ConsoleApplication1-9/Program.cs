c122c9d6dd6f04a69ed900fcbd67180369e499c7
samibhatti <abdulsami.bhatti@gmail.com>
2016-12-16 12:34:37 +0100

    1-9

---
+++ Projects/ConsoleApplication1-9/ConsoleApplication1-9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1_9
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Task<int> t = Task.Run(() =>
+            {
+                return 42;
+            });
+
+            Console.WriteLine(t.Result); // Display 42
+
+            Console.ReadKey();
+        }
+    }
+}
