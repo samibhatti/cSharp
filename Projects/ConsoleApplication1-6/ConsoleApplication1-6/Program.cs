3e34240e90e6c8be5418510bd99474f2b98bd64a
samibhatti <abdulsami.bhatti@gmail.com>
2016-12-16 11:36:37 +0100

    example 1-6

---
+++ Projects/ConsoleApplication1-6/ConsoleApplication1-6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication5
+{
+    public static class Program
+    {
+        public static ThreadLocal<int> _field =
+              new ThreadLocal<int>(() =>
+              {
+                  return Thread.CurrentThread.ManagedThreadId;
+              });
+
+        public static void Main()
+        {
+            new Thread(() =>
+            {
+                for (int x = 0; x < _field.Value; x++)
+                {
+                 
+                    Console.WriteLine("Thread A: {0}", x);
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int x = 0; x < _field.Value; x++)
+                {
+                  
+                    Console.WriteLine("Thread B: {0}", x);
+                }
+            }).Start();
+
+            Console.ReadKey();
+        }
+
+    }
+}
